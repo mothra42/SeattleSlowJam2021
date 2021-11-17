bc6e0edaaae4d6f9b2d3c3722cbf1c6d6ae3d7e0
Adam de Lorimier-Farmer <adam.a.farmer4129@gmail.com>
2021-11-16 16:13:10 -0800

    Adds Ghost Movement and Damage
    Adds logic to calculate and execute movement for the ghost. Adds a
    method to apply damage to the character when an overlap occurs.

--- SeattleSlowJam/Source/SeattleSlowJam/NPC/GhostEnemy.cpp
+++ SeattleSlowJam/Source/SeattleSlowJam/NPC/GhostEnemy.cpp
@@ -5,6 +5,7 @@
 #include "Components/CapsuleComponent.h"
 #include "Components/StaticMeshComponent.h"
 #include "GameFramework/FloatingPawnMovement.h"
+#include "../CabinCharacter.h"
 
 // Sets default values
 AGhostEnemy::AGhostEnemy()
@@ -13,10 +14,12 @@ AGhostEnemy::AGhostEnemy()
 	PrimaryActorTick.bCanEverTick = true;
 
 	CapsuleComponent = CreateDefaultSubobject<UCapsuleComponent>(TEXT("CapsuleComponent"));
+	CapsuleComponent->OnComponentBeginOverlap.AddDynamic(this, &AGhostEnemy::OnBeginOverlap);
 	RootComponent = CapsuleComponent;
 	
 	EnemyMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("EnemyMesh"));
 	EnemyMesh->SetupAttachment(CapsuleComponent);
+	EnemyMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 	MovementComponent = CreateDefaultSubobject<UFloatingPawnMovement>(TEXT("MovementComponent"));
 }
 
@@ -24,20 +27,51 @@ AGhostEnemy::AGhostEnemy()
 void AGhostEnemy::BeginPlay()
 {
 	Super::BeginPlay();
-	Amplitude = FMath::RandRange(MinAmplitude, MaxAmplitude);
+	SetAmplitude();
+	SetPeriod();
 }
 
 // Called every frame
 void AGhostEnemy::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
+	Cycle = CalculateSineCycle(DeltaTime);
+	SetActorLocation(GetActorLocation() + 
+		(FVector::ForwardVector * MovementSpeed) + 
+		(FVector::UpVector * Amplitude * FMath::Sin(Cycle))
+	);
 }
 
-// Called to bind functionality to input
-void AGhostEnemy::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+float AGhostEnemy::CalculateSineCycle(float DeltaTime)
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
+	return FMath::Fmod(((DeltaTime * 360.0) / Period) + Cycle, 360.0);
 }
 
+void AGhostEnemy::SetAmplitude()
+{
+	Amplitude = FMath::RandRange(MinAmplitude, MaxAmplitude);
+}
+
+void AGhostEnemy::SetPeriod()
+{
+	Period = FMath::RandRange(MinPeriod, MaxPeriod);
+}
+
+void AGhostEnemy::OnBeginOverlap(UPrimitiveComponent* OverlappedComp,
+	AActor* OtherActor,
+	UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex,
+	bool bFromSweep,
+	const FHitResult& SweepResult)
+{
+	ACabinCharacter* CabinCharacter = Cast<ACabinCharacter>(OtherActor);
+	if (CabinCharacter != nullptr)
+	{
+		ApplyDamage(CabinCharacter);
+	}
+}
+
+void AGhostEnemy::ApplyDamage(ACabinCharacter* CabinCharacter)
+{
+	CabinCharacter->TakeDamage();
+}
--- SeattleSlowJam/Source/SeattleSlowJam/NPC/GhostEnemy.h
+++ SeattleSlowJam/Source/SeattleSlowJam/NPC/GhostEnemy.h
@@ -26,21 +26,47 @@ protected:
 	class UFloatingPawnMovement* MovementComponent;
 
 	UPROPERTY(Category = "Movement", EditDefaultsOnly, BlueprintReadOnly)
-	float MinAmplitude;
+	float MinAmplitude = 2.0f;
 
 	UPROPERTY(Category = "Movement", EditDefaultsOnly, BlueprintReadOnly)
-	float MaxAmplitude;
+	float MaxAmplitude = 3.5f;
+
+	UPROPERTY(Category = "Movement", EditDefaultsOnly, BlueprintReadOnly)
+	float MinPeriod = 200.0f;
+
+	UPROPERTY(Category = "Movement", EditDefaultsOnly, BlueprintReadOnly)
+	float MaxPeriod = 300.0f;
+
+	UPROPERTY(Category = "Movement", EditDefaultsOnly, BlueprintReadOnly)
+	float MovementSpeed = 2.0f;
+
+	float Cycle = 0.0f;
 
 	float Amplitude;
 
+	float Period;
+
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	virtual float CalculateSineCycle(float DeltaTime);
+
+	void SetAmplitude();
+
+	void SetPeriod();
+
+	void ApplyDamage(class ACabinCharacter* CabinCharacter);
 
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	UFUNCTION()
+	void OnBeginOverlap(UPrimitiveComponent* OverlappedComp,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult);
 
 };
