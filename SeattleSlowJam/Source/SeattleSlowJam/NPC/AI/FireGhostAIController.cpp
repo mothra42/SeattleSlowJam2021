4b71afc1a9795263a6a7932c6b300c2090c41cc0
Adam de Lorimier-Farmer <adam.a.farmer4129@gmail.com>
2021-11-18 16:43:33 -0800

    Adds FireGhost AI class
    Adds an AI class for fire ghost that looks for the player character and
    pursues them, when they are in an acceptable radius.

---
+++ SeattleSlowJam/Source/SeattleSlowJam/NPC/AI/FireGhostAIController.cpp
@@ -0,0 +1,66 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FireGhostAIController.h"
+#include "Kismet/GameplayStatics.h"
+#include "../FireGhostEnemy.h"
+
+void AFireGhostAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//Set timer to check if player is within acceptable radius
+	GetWorld()->GetTimerManager().SetTimer(
+		TimerHandle_CheckForPlayerTimerExpired,
+		this,
+		&AFireGhostAIController::CheckIfPlayerInRadius,
+		PlayerCheckTimer,
+		true
+	);
+
+	//set timer to update the player's location for pursuit.
+	GetWorld()->GetTimerManager().SetTimer(
+		TimerHandle_UpdatePlayerLocationTimerExpired,
+		this,
+		&AFireGhostAIController::UpdateMoveTo,
+		UpdatePlayerLocationTimer,
+		true
+	);
+}
+
+void AFireGhostAIController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AFireGhostAIController::CheckIfPlayerInRadius()
+{
+	AFireGhostEnemy* ControlledFireGhost = Cast<AFireGhostEnemy>(GetPawn());
+	FVector PlayerLocation = UGameplayStatics::GetPlayerPawn(GetWorld(), 0)->GetActorLocation();
+	FVector ControlledPawnLocation = ControlledFireGhost->GetActorLocation();
+	float Distance = FMath::Abs((PlayerLocation - ControlledPawnLocation).Size());
+	if (Distance <= AcceptablePlayerRadius)
+	{
+		bShouldPursuePlayer = true;
+		ControlledFireGhost->SetActorTickEnabled(false);
+	}
+	else
+	{
+		bShouldPursuePlayer = false;
+		ControlledFireGhost->SetActorTickEnabled(true);
+	}
+}
+
+void AFireGhostAIController::PursuePlayer()
+{
+	if (bShouldPursuePlayer)
+	{
+		AActor* Player = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+		MoveToActor(Player);
+	}
+}
+
+void AFireGhostAIController::UpdateMoveTo()
+{
+	PursuePlayer();
+}
