1d70ca0a7f246b05fdcdcfbfe62330aae302adba
Adam de Lorimier-Farmer <adam.a.farmer4129@gmail.com>
2021-12-07 15:04:13 -0800

    Add C++ code to damge player from NPC Projectile
    Removes blueprint code that was handling player death from fire drop.
    Adds c++ code that calls the HandleDeath method in CabinCharacter class.

--- SeattleSlowJam/Source/SeattleSlowJam/NPC/EnemyProjectiles/FireDrop.h
+++ SeattleSlowJam/Source/SeattleSlowJam/NPC/EnemyProjectiles/FireDrop.h
@@ -25,4 +25,12 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	UFUNCTION()
+	void OnBeginOverlap(UPrimitiveComponent* OverlappedComp,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult);
+
 };
